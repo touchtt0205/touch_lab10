c348bda809480a6eb24505917d07453206adf983
touch <tttouch1122@gmail.com>
2022-12-27 15:59:11 +0700

    lab10_1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 int main(){	
+    double principle,interest,payback,PrevBalance;
+    
 	cout << "Enter initial loan: ";
+	cin>>principle;
 	cout << "Enter interest rate per year (%): ";
+	cin>>interest;
 	cout << "Enter amount you can pay per year: ";
+	cin>>payback;
 
+    
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -20,14 +26,25 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	PrevBalance = principle;
+	for(int i=1;1;i++){
+	    double Interest = PrevBalance*(interest/100),Total = PrevBalance + Interest,NewBa = Total - payback;
+	    if(NewBa<=0){
+	        payback = Total;
+	        NewBa = 0;
+	    }
+        cout << fixed << setprecision(2); 
+    	cout << setw(13) << left << i; 
+    	cout << setw(13) << left << PrevBalance;
+    	cout << setw(13) << left << Interest;
+    	cout << setw(13) << left << Total;
+       	cout << setw(13) << left << payback;
+    	cout << setw(13) << left << NewBa;
+    	cout << "\n";
+    	PrevBalance = NewBa;
+    	if(NewBa==0){
+    	    break;
+    	}
+	}
 	return 0;
 }
